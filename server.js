5c5f00a4459ea47deb6151bf9b45424c2cbae4e0
adamdasovich <adamdasovich@gmail.com>
2023-05-26 10:07:44 -0400

    fixed cors

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const productRoutes = require('./routes/product');
 const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
 const stripeRoutes = require('./routes/stripe');
+const corsOptions = require('./config/corsOptions');
 
 const cors = require('cors');
 
@@ -20,7 +21,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.static('build'));
 app.use(express.json());
