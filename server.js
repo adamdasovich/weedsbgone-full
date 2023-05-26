77b6df0f57e191950204dadfd4815e4cf75afce2
adamdasovich <adamdasovich@gmail.com>
2023-05-26 09:05:37 -0400

    preparing for deployment

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGO_URL)
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+
 app.use(express.static('build'));
 app.use(express.json());
 app.use('/api/users', userRoutes);
