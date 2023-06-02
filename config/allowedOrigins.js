4800762fa8a94021ce66b6dc65e7a025e6b012e2
adamdasovich <adamdasovich@gmail.com>
2023-06-02 08:07:43 -0400

    changed the Render settings

--- config/allowedOrigins.js
+++
@@ -1,5 +0,0 @@
-const allowedOrigins = [
-	'https://weedsbgone.onrender.com'
-];
-
-module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const productRoutes = require('./routes/product');
 const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
 const stripeRoutes = require('./routes/stripe');
-const corsOptions = require('./config/corsOptions');
-
 const cors = require('cors');
 
 dotenv.config();
@@ -21,7 +19,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.static('build'));
 app.use(express.json());
