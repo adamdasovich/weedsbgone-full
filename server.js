5fa2d816a84cb29b8ba44f4f5a34de2908713029
adamdasovich <adamdasovich@gmail.com>
2023-06-02 09:22:59 -0400

    Made changes for Render

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ mongoose.connect(process.env.MONGO_URL)
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+	origin: ['http://localhost:3000', 'https://weeds-front.onrender.com'],
+}));
 
 app.use(express.static('build'));
 app.use(express.json());
