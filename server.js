1df5727524c94c319a266c8bf4b711d0b8d2ff02
adamdasovich <adamdasovich@gmail.com>
2023-06-11 12:04:29 -0400

    added a server route

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+	res.send('API is running...');
+});
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
