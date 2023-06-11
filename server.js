b64ef4a5315fd0dee056d834a7d0e34a0dc77d17
adamdasovich <adamdasovich@gmail.com>
2023-06-11 10:09:24 -0400

    preparing for render

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
@@ -9,23 +8,16 @@ const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
 const stripeRoutes = require('./routes/stripe');
 const cors = require('cors');
+const path = require('path');
+
+const connectDB = require('./config/db');
 
 dotenv.config();
-
-// DB Config
-mongoose.connect(process.env.MONGO_URL)
-	.then(() => console.log('MongoDB Connected'))
-	.catch(err => console.log(err));
-
-const PORT = process.env.PORT || 5000;
-
-app.use(cors({
-	origin: ['http://localhost:3000', 'https://weeds-front.onrender.com'],
-}));
-
-
-app.use(express.static('build'));
+connectDB();
+app.use(cors());
 app.use(express.json());
+
+
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
@@ -33,6 +25,35 @@ app.use('/api/carts', cartRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/checkout', stripeRoutes);
 
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	})
+}
+
+const notFound = (req, res, next) => {
+	const error = new Error(`Not Found - ${req.originalUrl}`)
+	res.status(404)
+	next(error)
+}
+
+const errorHandler = (err, req, res, next) => {
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+	res.status(statusCode)
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+	})
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
